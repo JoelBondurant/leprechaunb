68ed9d65e545b6b3043f02cd24045d22d5380907
Joel Bondurant <joel@analyticobjects.com>
2019-09-27 12:47:45 +0000

    join cryptotwitter

--- btcd.sh
+++ btcd.sh
@@ -4,7 +4,7 @@ echo "Killing Bitcoin."
 kill -9 $(pgrep bitcoin)
 sleep 4
 echo "Startup..."
-nohup nice bitcoin-core.daemon -daemon -prune=550 -listen=0 -blocksonly=1 -dbcache=10 -maxorphantx=10 -maxmempool=10 -maxconnections=4 -maxsigcachesize=2 -rpcthreads=1 -par=1 &
+nohup nice bitcoin-core.daemon -daemon -prune=550 -listen=0 -blocksonly=1 -dbcache=10 -maxorphantx=10 -maxmempool=10 -maxconnections=4 -maxsigcachesize=2 -rpcthreads=1 -par=1 </dev/null >/dev/null 2>&1 &
 sleep 4
 echo "CPU limiting..."
 cpulimit --pid=$(pgrep bitcoin) --limit=80 --background
