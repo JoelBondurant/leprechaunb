147ece2317f0c088c7e8eab1990170d35a1e8e88
Joel Bondurant <joel@analyticobjects.com>
2019-09-27 10:17:34 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-echo "Leprechaunb installing."
+echo "Leprechaun B installing."
+sudo groupadd -g 7979 leprechaunb
+sudo useradd -u 7979 -g 7979 leprechaunb
 PY_USER_SITES=$(python3 -m site --user-site)
 echo Python3 User Sites: ${PY_USER_SITES}
 mkdir -p ${PY_USER_SITES}
 echo $PWD > ${PY_USER_SITES}/leprechaunb.pth
-echo "Leprechaunb installed."
+echo "Leprechaun B installed."
