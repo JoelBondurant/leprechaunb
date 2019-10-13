afc0f683aa15dce745f28fbf0ca42e8cdc1b2978
Joel Bondurant <joel@analyticobjects.com>
2019-10-13 22:16:28 +0000

    service unit tweaks

--- bitcoin/install.sh
+++ bitcoin/install.sh
@@ -1,7 +1,10 @@
+cd ./bitcoin
 vn="0.18.1"
 fn="bitcoin-"${vn}"-x86_64-linux-gnu.tar.gz"
 echo "Installing Bitcoin "${vn}" from "${fn}
 curl -O https://bitcoin.org/bin/bitcoin-core-${vn}/${fn}
 tar -xzvf ${fn} -C .
+mv bitcoin-${vn} bitcoin
 rm ${fn}
+cd ..
 echo "Bitcoin installed"
