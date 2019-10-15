2f7af2f78d41b0e4ff25791054525eb832cab250
Joel Bondurant <joel@analyticobjects.com>
2019-10-15 07:06:17 +0000

    gold pot adds

--- wap/views/pot.py
+++ wap/views/pot.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import json
+
+
 import cachetools.func
 import ecdsa
 
@@ -61,3 +64,52 @@ def gold():
 		return make_response(redirect("/login", code=302))
 
 	return make_response(render_template("pot_gold.html", **content))
+
+
+@pot_blueprint.route("/pot/gold/new", methods=["POST"])
+def new_gold():
+
+	if ("uid" not in request.cookies) or ("ukey_token" not in request.cookies):
+		return make_response(redirect("/login", code=302))
+
+	uid = request.cookies.get("uid")
+	assert len(uid) == 16
+	ukey_token = request.cookies.get("ukey_token")
+
+	if not auth.verify_ukey_token(uid, ukey_token):
+		return make_response(redirect("/login", code=302))
+
+	datebin = request.form.get("datebin")
+	grams = request.form.get("grams")
+	source = request.form.get("source")
+	latitude = request.form.get("latitude")
+	longitude = request.form.get("longitude")
+	ownemail = request.form.get("ownemail")
+	benemail = request.form.get("benemail")
+	note = request.form.get("note")
+
+	udb = sqlite.KV("udb")
+	key = f"pot_gold_{uid}"
+	pot_json = udb.get(key)
+	if pot_json is None:
+		pot = []
+	else:
+		pot = json.loads(pot_json)
+	pot += [
+		{
+			"datebin": datebin,
+			"grams": grams,
+			"source": source,
+			"latitude": latitude,
+			"longitude": longitude,
+			"ownemail": ownemail,
+			"benemail": benemail,
+			"note": note,
+		}
+	]
+	pot_json = json.dumps(pot)
+	udb.put(key, pot_json)
+
+	resp = make_response(redirect("/pot", code=302))
+	return resp
+
