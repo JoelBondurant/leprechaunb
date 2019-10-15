87c7677644a996a96e251825f3d06379675e82e1
Joel Bondurant <joel@analyticobjects.com>
2019-10-15 02:07:49 +0000

    not storing ukey, only a hash. in prep to encrypt user data with ukey.

--- util/auth.py
+++ util/auth.py
@@ -3,6 +3,7 @@ Home rolled auth, never trust anyone.
 """
 
 import base64
+import hashlib
 import random
 import secrets
 import time
@@ -20,16 +21,19 @@ def random_sleep(base=0.1, addl=0.1):
 	time.sleep(base + addl*random.random())
 
 
+deviceid_len = 32
 def gen_deviceid():
-	return secrets.token_hex(16)
+	return secrets.token_hex(deviceid_len//2)
 
 
+uid_len = 16
 def gen_uid():
-	return secrets.token_hex(8)
+	return secrets.token_hex(uid_len//2)
 
 
+ukey_len = 16
 def gen_ukey():
-	return secrets.token_hex(8)
+	return secrets.token_hex(ukey_len//2)
 
 
 @cachetools.func.ttl_cache(ttl=12*3600)
@@ -48,16 +52,25 @@ def get_ukey_signature_key():
 	return sk
 
 
+def hash_ukey(akey):
+	salt0 = "19c6dc34e838f330"
+	salt1 = hashlib.sha256((salt0 + akey).encode()).hexdigest()
+	hashedup = hashlib.sha256((salt0 + akey).encode()).hexdigest()
+	hashedup2x = hashlib.sha256((salt1 + hashedup).encode()).hexdigest()
+	return hashedup2x[:ukey_len]
+
+
 def gen_ukey_token(uid, ukey, new_ukey=False):
 	"""
 	Generate a token based on uid after ukey is checked.
 	"""
 	random_sleep()
 	if new_ukey:
-		sqlite.KV("udb").put(f"ukey_{uid}", ukey)
+		ukey_hash = hash_ukey(ukey)
+		sqlite.KV("udb").put(f"ukey_hash_{uid}", ukey_hash)
 	else:
-		udb_ukey = sqlite.KV("udb").get(f"ukey_{uid}")
-		assert udb_ukey == ukey
+		udb_ukey_hash = sqlite.KV("udb").get(f"ukey_hash_{uid}")
+		assert udb_ukey_hash == ukey_hash
 	sk = get_ukey_signature_key()
 	ukey_token = sk.sign(uid.encode())
 	ukey_token_b64 = base64.b64encode(ukey_token).decode()
