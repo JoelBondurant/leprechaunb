660ccb944f65fec3f0e0960a4c19fd4ba7feeb8d
Joel Bondurant <joel@analyticobjects.com>
2019-10-19 04:13:10 +0000

    noclue

--- wap/ws/js/util.js
+++ wap/ws/js/util.js
@@ -11,6 +11,22 @@ function dateTime(id) {
 }
 
 
+function toBase64(blob) {
+	return btoa(encodeURIComponent(blob).replace(/%([0-9A-F]{2})/g,
+		function toSolidBytes(match, p1) {
+			return String.fromCharCode("0x" + p1);
+		}
+	));
+}
+
+
+function fromBase64(msg) {
+	return decodeURIComponent(atob(msg).split("").map(function(c) {
+		return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+	}).join(""));
+}
+
+
 function sha256(msg) {
 	return crypto.subtle.digest("SHA-256", (new TextEncoder()).encode(msg));
 }
@@ -27,20 +43,18 @@ function pubSalt() {
 
 
 function pubIV() {
-	return new Uint8Array([79, 159, 147, 28, 195, 193, 197, 30, 25, 127, 1, 66]);
+	return new Uint8Array([79, 83, 33, 128, 195, 193, 197, 3, 25, 127, 20, 9]);
 }
 
 
 async function padString(msg) {
-	strLen = msg.length;
-	padding = await btoa(sha512(msg + pubSalt()));
+	padding = await toBase64(sha512(msg + pubSalt()));
 	return msg + padding.repeat(10);
 }
 
 
 async function unpadString(msg) {
 	return msg.slice(0, -240);
-
 }
 
 
@@ -62,25 +76,29 @@ async function deriveKey(akey0) {
 	return akey;
 }
 
+
 async function encrypt(msg, akey) {
 	/*wip*/
 	aesgcm = aesgcmParams();
 	akey = await deriveKey(akey);
+	msg = toBase64(msg);
 	msg = await padString(msg);
-	msg = (new TextEncoder()).encode(msg).buffer;
+	msg = (new TextEncoder()).encode(msg);
 	encr = await crypto.subtle.encrypt(aesgcm, akey, msg);
-	return btoa(encr);
+	return toBase64(encr);
 }
+
 
 async function decrypt(msg, akey) {
 	/*wip*/
 	aesgcm = aesgcmParams();
 	akey = await deriveKey(akey);
-	//msg = (new TextEncoder()).encode(atob(msg)).buffer;
-	msg = Uint8Array.from(atob(msg), c => c.charCodeAt(0));
-	decr = await crypto.subtle.decrypt(aesgcm, akey, msg);
+	msg1 = fromBase64(msg);
+	msg2 = Uint8Array.from(msg1, c => c.charCodeAt(0));
+	decr = await crypto.subtle.decrypt(aesgcm, akey, msg2);
+	//decr = fromBase64(decr)
 	//decr = await unpadString(decr);
-	return decr;
+	return [msg, msg1, msg2, decr];
 }
 
 
