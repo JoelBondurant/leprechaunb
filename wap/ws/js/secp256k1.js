849c1073ca0b87c4cab4d10e647232d15471fdfc
Joel Bondurant <joel@analyticobjects.com>
2019-11-08 06:10:15 +0000

    boom

--- wap/ws/js/secp256k1.js
+++ wap/ws/js/secp256k1.js
@@ -5,18 +5,10 @@ Secp256k1 - dev wip.
 
 
 /*
-secp256k1 group order.
+secp256k1 group order of the elliptic addition.
 */
 function ellipticOrder() {
 	return 115792089237316195423570985008687907853269984665640564039457584007908834671663n;
-}
-
-
-/*
-secp256k1 generator point order.
-*/
-function generatorOrder() {
-	return 115792089237316195423570985008687907852837564279074904382605163141518161494337n;
 }
 
 
@@ -31,7 +23,38 @@ function generatorPoint() {
 
 
 /*
-Elliptic field addition.
+secp256k1 cyclic group order of elliptic scalar multiplication or the generator point.
+G*1, G*2, ...G*n ~ Zn
+G * 5 = G + G + G + G + G = (G * 2) * 2 + G
+*/
+function generatorOrder() {
+	return 115792089237316195423570985008687907852837564279074904382605163141518161494337n;
+}
+
+
+/*
+Elliptic point doubling.
+*/
+function ellipticDouble(a) {
+	if (a == null) {
+		return null;
+	}
+	if (a[1] == 0n) {
+		return null;
+	}
+	G = ellipticOrder();
+	lam = ((3n * a[0] * a[0]) * invMod(2n * a[1], G));
+	x = (lam*lam - 2n*a[0]) % G;
+	y = (lam*(a[0] - x) - a[1]) % G;
+	if (y < 0n) {
+		y = y + G;
+	}
+	return [x, y];
+}
+
+
+/*
+Elliptic addition.
 */
 function ellipticAdd(a, b) {
 	if (a == null) {
@@ -39,6 +62,9 @@ function ellipticAdd(a, b) {
 	}
 	if (b == null) {
 		return a;
+	}
+	if (a == b) {
+		return ellipticDouble(a);
 	}
 	G = ellipticOrder();
 	lam = ((b[1] - a[1]) * invMod(b[0] - a[0], G)) % G;
@@ -49,40 +75,31 @@ function ellipticAdd(a, b) {
 
 
 /*
-Elliptic point doubling.
+Elliptic scalar multiplication.
 */
-function ellipticDouble(a) {
-	if (a == null) {
-		return a;
-	}
-	G = ellipticOrder();
-	lam = ((3n * a[0] * a[0]) * invMod(2n * a[1], G)) % G;
-	x = (lam*lam - 2n*a[0]) % G;
-	y = (lam*(a[0] - x) - a[1]) % G;
-	return [x, y];
-}
-
-
-/*
-Elliptic field scalar multiplication.
-*/
-function ellipticMultiply(b, start=0) {
-	bbin = b.toString(2);
+function ellipticMultiply(b) {
 	a = generatorPoint();
-	if (start==0) {
-		c = null;
-	} else if (start=1) {
-		c = a;
-	} else if (start=2) {
-		c = ellipticDouble(a);
-	}
+	bbin = b.toString(2);
+	c = a;
 	for (i=0; i<256; i++) {
+		if (bbin[i] == null) {
+			break;
+		}
 		if (bbin[i] == "1") {
 			c = ellipticAdd(c, a);
 		}
 		a = ellipticDouble(a);
 	}
 	return c;
+}
+
+
+/*
+Is point on secp256k1?
+*/
+function onCurve(a) {
+	G = ellipticOrder();
+	return (((a[1]*a[1] - a[0]*a[0]*a[0] - 7n) % G) == 0n);
 }
 
 
@@ -104,9 +121,6 @@ Secp256k1 public keygen.
 */
 function publicKey(privKey, index=0) {
 	pubKey = ellipticMultiply(privKey);
-	pubKey0 = pubKey[0].toString(16);
-	pubKey1 = pubKey[1].toString(16);
-	pubKey = [pubKey0, pubKey1];
 	return pubKey;
 }
 
